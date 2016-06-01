185f5016021a9b4eae39bae3bcb20a9eee5c624d
Manel Simon <manelizzard@gmail.com>
2016-06-01 22:50:29 +0200

    * Project initialization. * Menus package add

---
+++ Laser Defender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+/// <summary>
+/// Level manager class used to handle scene changes.
+/// </summary>
+public class LevelManager : MonoBehaviour {
+
+	/// <summary>
+	/// Loads the desired scene.
+	/// </summary>
+	/// <param name="name">The name of the scene as string.</param>
+	public void LoadLevel(string name) {
+		SceneManager.LoadScene (name);
+	}
+
+	/// <summary>
+	/// Quits the application (if application can be quitted).
+	/// </summary>
+	public void QuitRequest() {
+		Application.Quit ();
+	}
+
+	/// <summary>
+	/// Loads the next level set in the BuildSettings
+	/// </summary>
+	public void LoadNextLevel() {
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Singleton Music Player persisted among scenes
+/// </summary>
+public class MusicPlayer : MonoBehaviour {
+
+	/// <summary>
+	/// The instance (singleton)
+	/// </summary>
+	static MusicPlayer instance = null;
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake() {
+
+		// - If we already created a MusicPlayer, destroy the current one
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			// - The first time this script is created, save its instance
+			instance = this;
+
+			// - Avoid this object to be destroyed along the scenes
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+}
