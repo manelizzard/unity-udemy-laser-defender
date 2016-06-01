6c92b17baaaa753f83f7be12e6840cb5fabbb1ce
Manel Simon <manelizzard@gmail.com>
2016-06-01 23:11:14 +0200

    * Player shift moving left and right

---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15.0f;
+
+	void Update () {
+	
+		Vector3 currentPosition = gameObject.transform.position;
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			// - Move left
+			transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			// - Move right
+			transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+		}
+	}
+}
