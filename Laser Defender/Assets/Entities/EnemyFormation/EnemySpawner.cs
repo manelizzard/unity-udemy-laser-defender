9f3e8e0f855db67a615fb08949af9d300b19c505
Manel Simon <manelizzard@gmail.com>
2016-06-20 23:45:55 +0200

    ## Section 6, Lecture 106 * Drawing gizmos * Enemy spawner now spawns enemies in a formation.

---
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField] GameObject enemyPrefab;
+
+	void Start () {
+		// - Instantiate enemies and attach them under the EnemyFormation positions
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate (enemyPrefab, child.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
