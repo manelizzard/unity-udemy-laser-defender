76ebe402a85eae94a345543a3344d535fd46bb4e
Manelizzard <manelizzard@gmail.com>
2016-06-22 16:53:22 +0200

    ### Section 6, Lecture 110 * Projectiles collide and destroy themselves and enemies (if its health reaches 0)

---
+++ Laser Defender/Assets/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public float health;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+
+		// - Check if a Projectile has hit the enemy
+		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
+		if (missile != null) {
+			// - Make the enemy get the damage
+			float damage = missile.damage;
+			TakeDamage (damage);
+
+			// - Notify projectile to destroy itself
+			missile.Hit();
+		}
+	}
+
+	/// <summary>
+	/// Takes the damage from the projectile.
+	/// </summary>
+	/// <param name="damage">Damage.</param>
+	void TakeDamage(float damage) {
+		health -= damage;
+
+		// - If health reaches 0, destroy itself
+		if (health <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Laser Defender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float damage;
+
+	/// <summary>
+	/// Gets the damage this projectile does when hit.
+	/// </summary>
+	/// <returns>The damage.</returns>
+	public float GetDamage() {
+		return damage;
+	}
+
+	/// <summary>
+	/// Notify Projectile has hit something. Destroy itself.
+	/// </summary>
+	public void Hit() {
+		Destroy (gameObject);
+	}
+}
